2c69a318e8515bb80f398f36437b3e6267a81bbb
Poonam Poudel <mepoudyal@gmail.com>
2026-01-01 21:38:56 +0545

    hw 5

--- lec05/homework5.py
+++ lec05/homework5.py
@@ -16,7 +16,8 @@ def center_of_gravity(x):
     @result:
     c (scalar): x's center of gravity
     '''
-    c = 0  # change this line
+    n = np.arange(len(x))
+    c = np.sum(n * x) / np.sum(x)
     return c
 
 def matched_identity(x):
@@ -30,8 +31,7 @@ def matched_identity(x):
     @result:
     I (array): a 2d numpy array: an NxN identity matrix
     '''
-    I =  0 # change this line
-    return I
+    return np.identity(len(x))
 
 def sine_and_cosine(t_start, t_end, t_steps):
     '''
@@ -48,9 +48,6 @@ def sine_and_cosine(t_start, t_end, t_steps):
     x (array of length t_steps): cos(t)
     y (array of length t_steps): sin(t)
     '''
-    # change these lines
-    t = 0 
-    x = 0
-    y = 0
-    # end changes here
+    t = np.linspace(t_start, t_end, t_steps)
+    x, y = np.cos(t), np.sin(t)
     return t, x, y

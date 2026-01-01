3a8ca717666ddc85c9cb419f129b9a7b3af3a5d8
Poonam Poudel <mepoudyal@gmail.com>
2026-01-01 21:22:37 +0545

    hw 2

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -12,5 +12,20 @@ def arithmetic(x, y):
     isinstance(y,str)   return x+y         return str(x)+y
     isinstance(y,float) return x*int(y)    return x*y
     """
-    return 0
+def arithmetic(x, y):
+    """
+    Modify this code so that it performs one of four possible functions, 
+    as specified in the following table:
 
+                        isinstance(x,str)  isinstance(x,float)
+    isinstance(y,str)   return x+y         return str(x)+y
+    isinstance(y,float) return x*int(y)    return x*y
+    """
+    if isinstance(x,str) and isinstance(y,str):
+        return x + y
+    elif isinstance(x,str) and isinstance(y,float):
+        return x * int(y)
+    elif isinstance(x,float) and isinstance(y,str):
+        return str(x) + y
+    elif isinstance(x,float) and isinstance(y,float):
+        return float(x) * float(y)
